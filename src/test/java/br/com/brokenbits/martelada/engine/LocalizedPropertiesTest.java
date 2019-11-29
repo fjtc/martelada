485557b7e82fcea7cb36b591fbccb3df5ef1dc96
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2019-11-29 02:10:44 -0300

    Applying the license to all source files.

--- src/test/java/br/com/brokenbits/martelada/engine/LocalizedPropertiesTest.java
+++ src/test/java/br/com/brokenbits/martelada/engine/LocalizedPropertiesTest.java
@@ -1,3 +1,20 @@
+/**
+ * martelada - a very simple Java resource file editor 
+ * Copyright (C) 2019 Fabio Jun Takada Chino
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package br.com.brokenbits.martelada.engine;
 
 import static org.junit.Assert.*;
@@ -27,7 +44,7 @@ public class LocalizedPropertiesTest {
 		p.load(file);
 		
 		assertEquals(1, p.getProperties().size());
-		assertEquals("sample", p.getProperties().get("sample"));
+		assertEquals("file", p.getProperties().get("file"));
 	}
 
 	@Test
